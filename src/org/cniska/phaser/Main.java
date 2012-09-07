bf614c827924d74d96803f697ea59bc726f490d9
Crisu83 <ChristofferNiska@gmail.com>
2012-09-07 22:48:15 +0300

    Init project

---
+++ src/org/cniska/phaser/Main.java
@@ -0,0 +1,15 @@
+package org.cniska.phaser;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Main extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
